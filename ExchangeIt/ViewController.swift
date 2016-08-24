1943bbc639d132faeafb491c33ffcee0070a75fc
SunFloweri <irinakalashnikova@gmail.com>
2016-08-23 22:10:41 -0400

    Initial commit

---
+++ ExchangeIt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ExchangeIt
+//
+//  Created by Irina Kalashnikova on 8/23/16.
+//  Copyright © 2016 Irina Ernst. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
